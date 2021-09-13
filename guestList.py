085055643eea416e14cd42fa94ba2e24dae7a024
AaronKaddu <aaronmuzira@gmail.com>
2021-09-13 20:20:05 +0300

    # Making a list of Guests to Invite

---
+++ guestList.py
@@ -0,0 +1,12 @@
+# Making a list of Guests to Invite
+
+myGuests = ['Mike', 'Paul', 'Rogers', 'Philliam', 'Apollo', 'John']
+
+# Inviting my Guests
+# Guest1 = 'Mike'
+print("Dear " + myGuests[0].title() + " \nYou are invited for my party that i have  \n\t")
+print("Dear " + myGuests[1].title() + " \nYou are invited for my party that i have organized\n\t")
+print("Dear " + myGuests[2].title() + " \nYou are invited for my party that i have organized\n\t")
+print("Dear " + myGuests[3].title() + " \nYou are invited for my party that i have organized\n\t")
+print("Dear " + myGuests[4].title() + " \nYou are invited for my party that i have organized\n\t")
+print("Dear " + myGuests[5].title() + " \nYou are invited for my party that i have organized\n\t")
